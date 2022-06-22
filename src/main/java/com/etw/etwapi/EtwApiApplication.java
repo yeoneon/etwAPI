c04d70dc07794be4fc46bd71b96e338a17dff4a6
yeoneon <yeoneon0524@gmail.com>
2022-06-22 23:00:36 +0900

    init

---
+++ src/main/java/com/etw/etwapi/EtwApiApplication.java
@@ -0,0 +1,13 @@
+package com.etw.etwapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtwApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtwApiApplication.class, args);
+	}
+
+}
